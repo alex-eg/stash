709ed4c7c4cf066ff5f062c1429f66ab9d9ae3b7
taptap <alexey.e.egorov@gmail.com>
2015-09-02 13:44:32 +0300

    add user class

--- user.lisp
+++ user.lisp
@@ -1,7 +1,14 @@
-(in-package :model)
+(in-package :stash.model)
 
 (defun string->hash (string)
   (ironclad:byte-array-to-hex-string
    (ironclad:digest-sequence
     :sha256
     (flexi-streams:string-to-octets string))))
+
+(defclass user (mongo-storable)
+  ((name :initarg :name)
+   (email :initarg :email)
+   (handle :initarg :handle)
+   (password :initarg :password)
+   (friend-list :initarg :friend-list)))

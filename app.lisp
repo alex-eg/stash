6b8b447d29ec09058fe5d54f79ff1989d03f95c8
taptap <alexey.e.egorov@gmail.com>
2015-09-02 13:11:25 +0300

    add ningle routing descriptions

--- app.lisp
+++ app.lisp
@@ -1,1 +1,25 @@
-(in-package :app)
+(in-package :stash)
+
+(defun install-routes (app)
+  (setf (ningle:route app "/")
+        #'stash.views::main-page)
+  (setf (ningle:route app "/login")
+        #'stash.views::login-page)
+  (setf (ningle:route app "/logout")
+        #'logout))
+
+(defun logout (params)
+  ())
+
+(defun print-hash (hash)
+  (loop
+     for k being the hash-key of hash
+       using (hash-value v) do (format t "~A ~A~%" k v)))
+
+(defun start ()
+  (setf (cl-who:html-mode) :html5)
+  (let ((app (make-instance 'ningle:<app>)))
+    (install-routes app)
+    (clack:clackup
+     (lack:builder :session
+                   app))))

f17cc73d855782da94524861362ba30eef779d9f
alex_e <alexey.e.egorov@gmail.com>
2015-09-03 16:03:53 +0300

    add toy authorization support

--- app.lisp
+++ app.lisp
@@ -1,10 +1,26 @@
 (in-package :stash)
 
+(defvar *user*
+  (make-instance 'stash.model::user
+                 :login "user"
+                 :handle "Haru6aTop"
+                 :password (stash.model::string->hash "TpyCuKu")))
+
 (defun install-routes (app)
   (setf (ningle:route app "/")
         #'stash.views::main-page)
   (setf (ningle:route app "/login")
         #'stash.views::login-page)
+  (setf (ningle:route app "/login" :method :post)
+        #'(lambda (params)
+            (let ((login (cdr (assoc "login" params :test #'string=)))
+                  (password (cdr (assoc "password" params :test #'string=)))
+                  (user *user*))
+              (if (and (string= (stash.model::user-login user) login)
+                       (string= (stash.model::string->hash password)
+                                (stash.model::user-password user)))
+                  "You're authorized! Hooray!"
+                  "I don't know you. Go away."))))
   (setf (ningle:route app "/logout")
         #'logout))
 

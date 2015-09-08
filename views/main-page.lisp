3d7825b2acbc4c3d4b27e879e4286a236f03a99f
taptap <alexey.e.egorov@gmail.com>
2015-09-08 23:48:35 +0300

    update login view

--- views/main-page.lisp
+++ views/main-page.lisp
@@ -16,9 +16,10 @@
 
 ;;; route "/login"
 (define-view login-page (params)
-  (:form :id "login"
-         :action "./login"
-         :method "post"
-         (str "Login") (:input :type "text" :name "login") (:br)
-         (str "Password") (:input :type "password" :name "password") (:br)
-         (:input :type "submit" :value "Log in")))
+  (:div :class "login" :align "center"
+        (:form :class "login"
+               :action "./login"
+               :method "post"
+               (str "Login") (:input :type "text" :name "login") (:br)
+               (str "Password") (:input :type "password" :name "password") (:br)
+               (:input :type "submit" :value "Log in"))))

8509888e8923e0e7784052f2b65f941c6ef3cb51
taptap <alexey.e.egorov@gmail.com>
2015-09-09 00:53:27 +0300

    fix css generation functions

--- static/css.lisp
+++ static/css.lisp
@@ -1,14 +1,18 @@
 (in-package :stash.views)
 
-(defun generate-css (lass-sheet path)
-  (with-open-file (css-file path :direction :output
-                            :if-exists :overwrite))
-  (lass:write-sheet lass-sheet css-file))
+(defun generate-css (lass-blocks path)
+  (with-open-file (css-file (merge-pathnames *default-pathname-defaults* path)
+                            :direction :output
+                            :if-does-not-exist :create
+                            :if-exists :overwrite)
+    (lass:write-sheet (lass:compile-sheet lass-blocks)
+                      :stream css-file)))
 
-(defun general-css)
-(generate-css
- '(div.login
-   :background "#0088EE"
-   :boreder-radius "12px"
-   :padding "20px")
- #P"./main.css")
+(defun generate-general-css ()
+  (generate-css
+   '(div.login
+     :border "2px solid #8AC007"
+     :background "#0088EE"
+     :boreder-radius "12px"
+     :padding "20px")
+   #P"./main.css"))

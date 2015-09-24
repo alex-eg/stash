c78b909f2df85bfe98cdc8bc9b34e5447299c26e
alex_e <alexey.e.egorov@gmail.com>
2015-09-24 17:29:05 +0300

    add compile-time definition of project root path
    using computed path as root for static files such as css' or scripts

--- app.lisp
+++ app.lisp
@@ -40,15 +40,25 @@
      for k being the hash-key of hash
        using (hash-value v) do (format t "~A ~A~%" k v)))
 
+;;; HARDCODE DEVELOPMENT
+(eval-when (:compile-toplevel)
+  (defparameter *source-location*
+    (make-pathname
+     :directory (pathname-directory *compile-file-truename*)
+     :device (pathname-device *compile-file-truename*))))
+
+
 (defun start ()
   (generate-css)
   (setf (cl-who:html-mode) :html5)
   (let ((app (make-instance 'ningle:<app>))
-        (config (make-instance 'config :collection "config")))
+        (config (load-config-from-file #P"default-config.lisp")))
     (install-routes app)
     (clack:clackup
      (lack:builder :session
                    (:static
-                    :path "/static"
-                    :root *default-pathname-defaults*)
+                    :path "/static/"
+                    :root (merge-pathnames (static-path config)
+                                           (or *source-location* ; development setting
+                                               (root-path config))))
                    app))))

ee0bd976f71fb9e99e2b52bf0336605b2f30d86a
taptap <alexey.e.egorov@gmail.com>
2015-10-01 23:18:44 +0300

    add script extraction and embedding

--- views/view.lisp
+++ views/view.lisp
@@ -3,15 +3,20 @@
 (defmacro define-view (name parameters &body body)
   (check-type name symbol "symbol")
   (check-type parameters list "lambda list")
-  (alexandria:with-gensyms (s)
-    `(defun ,name ,parameters
-       (with-html-output-to-string (,s nil :prologue t :indent t)
-         (:head
-          (:meta :charset "UTF-8")
-          (:link :rel "stylesheet" :type "text/css" :href "./static/main.css"))
-         (:body
-          ,@body)
-         ,s))))
+  (let ((scripts (loop
+                    :for entry :in body
+                    :if (eql (car entry) :script)
+                    :collect entry)))
+    (alexandria:with-gensyms (s)
+      `(defun ,name ,parameters
+         (with-html-output-to-string (,s nil :prologue t :indent t)
+           (:head
+            (:meta :charset "UTF-8")
+            (:link :rel "stylesheet" :type "text/css" :href "./static/main.css")
+            ,@scripts)
+           (:body
+            ,@body)
+           ,s)))))
 
 (define-view hello-page (params)
   (str (header params))

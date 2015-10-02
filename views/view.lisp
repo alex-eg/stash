5a4f02d17112ca8ff011cbd656a6050005c67c73
alex_e <alexey.e.egorov@gmail.com>
2015-10-02 14:48:55 +0300

    add more fun to greeting page

--- views/view.lisp
+++ views/view.lisp
@@ -32,5 +32,10 @@
 
 (define-view hello-page (params)
   (str (header params))
+  (:script (format nil "
+function greet() {
+    document.getElementById(\"hello-p\").innerHTML += ~S;
+}" (cdr (assoc :name params))))
   (:div :id "hello-area"
-        (:p (:b (fmt "Hello, ~A" (cdr (assoc :name params)))))))
+        (:p (:b :id "hello-p" (str "Hello,")))
+        (:button :type "button" :onclick "greet()" (str "Greet!"))))

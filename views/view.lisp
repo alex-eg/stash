7c6c903e1f4678eafa60775cbbe545becb553229
taptap <alexey.e.egorov@gmail.com>
2015-10-06 20:14:12 +0300

    move needed for macroexpansion functions to labels form

--- views/view.lisp
+++ views/view.lisp
@@ -1,34 +1,33 @@
 (in-package :stash.views)
 
-(defun collect-tags (body tag)
-  (loop :for entry :in body
-     :if (eql (car entry) tag)
-     :collect entry))
-
-(defun remove-tags (body tag)
-  (remove tag body :test #'eql :key #'car))
 
 (defmacro define-view (name parameters &body body)
-  (check-type name symbol "symbol")
-  (check-type parameters list "lambda list")
-  (let* ((scripts (loop :for script :in (collect-tags body :script)
-                     :collect `(:script (str ,(cadr script)))))
-         (body (remove-tags body :script))
-         (css-list (loop
-                      :for css :in (collect-tags body :css)
-                      :collect `(:link :rel "stylesheet" :type "text/css" :href ,(cadr css))))
-         (body (remove-tags body :css)))
-    (alexandria:with-gensyms (s)
-      `(defun ,name ,parameters
-         (with-html-output-to-string (,s nil :prologue t :indent t)
-           (:html (:head
-                   (:meta :charset "UTF-8")
-                   (:link :rel "stylesheet" :type "text/css" :href "/static/main.css")
-                   ,@css-list
-                   ,@scripts)
-                  (:body
-                   ,@body))
-           ,s)))))
+  (labels ((collect-tags (body tag)
+             (loop :for entry :in body
+                   :if (eql (car entry) tag)
+                     :collect entry))
+           (remove-tags (body tag)
+             (remove tag body :test #'eql :key #'car)))
+    (check-type name symbol "symbol")
+    (check-type parameters list "lambda list")
+    (let* ((scripts (loop :for script :in (collect-tags body :script)
+                          :collect `(:script (str ,(cadr script)))))
+           (body (remove-tags body :script))
+           (css-list (loop
+                       :for css :in (collect-tags body :css)
+                       :collect `(:link :rel "stylesheet" :type "text/css" :href ,(cadr css))))
+           (body (remove-tags body :css)))
+      (alexandria:with-gensyms (s)
+        `(defun ,name ,parameters
+           (with-html-output-to-string (,s nil :prologue t :indent t)
+             (:html (:head
+                     (:meta :charset "UTF-8")
+                     (:link :rel "stylesheet" :type "text/css" :href "/static/main.css")
+                     ,@css-list
+                     ,@scripts)
+                    (:body
+                     ,@body))
+             ,s))))))
 
 (define-view hello-page (params)
   (str (header params))

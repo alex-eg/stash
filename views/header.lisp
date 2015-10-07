774d28c62b7a5dff181435f4dfc895f6e18033c9
alex_e <alexey.e.egorov@gmail.com>
2015-10-07 18:03:18 +0300

    remove unused html from header view

--- views/header.lisp
+++ views/header.lisp
@@ -3,9 +3,8 @@
 (defun header (params)
   (declare (ignore params))
   (with-html-output-to-string (s nil :indent t)
-    (:div :id "header"
-          (:div :id "row"
-                (:div :id "left"
-                      (if (gethash :authorized (ningle:context :session))
-                          (htm (:a :href "/logout" (str "Log out")))
-                          (htm (:a :href "/login" (str "Log In")))))))))
+    (if (gethash :authorized (ningle:context :session))
+        (progn
+          (htm (:a :href "/logout" (str "Log out"))
+               (:a :href "/newentry" (str "New entry"))))
+        (progn (htm (:a :href "/login" (str "Log In")))))))

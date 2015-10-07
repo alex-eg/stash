51e4b01ee1f0ac4e3a73333d2e83161c36c527e3
alex_e <alexey.e.egorov@gmail.com>
2015-10-07 18:03:27 +0300

    reindent

--- views/main-page.lisp
+++ views/main-page.lisp
@@ -3,6 +3,6 @@
 (define-view main-page (params)
   (esc (format nil "~{~S~%~}~%"
                (loop :for key :being :the :hash-keys :of ningle:*context*
-                     :using (:hash-value value)
-                     :collect (list key value))))
+                  :using (:hash-value value)
+                  :collect (list key value))))
   (str (header params)))

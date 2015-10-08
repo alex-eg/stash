f01d919c20902c757b356a27e6c9d31456492a2b
alex_e <alexey.e.egorov@gmail.com>
2015-10-08 17:59:16 +0300

    improve view for new post entry

--- views/newentry.lisp
+++ views/newentry.lisp
@@ -2,4 +2,8 @@
 
 (define-view newentry (params)
   (str (header params))
-  (:form (:input :type "text")))
+  (:form :action "./newentry"
+         :method "post"
+         (:input :type "text" :name "caption")
+         (:textarea :cols "80" :rows "20" :name "post-body")
+         (:input :type "submit" :value "Create new post")))

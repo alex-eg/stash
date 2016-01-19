07b5b3dbe4e764c3659ad14ee302d35e28088a84
taptap <alexey.e.egorov@gmail.com>
2016-01-19 17:36:52 +0300

    make use of convenient macro in post-list view function

--- views/posts-list-page.lisp
+++ views/posts-list-page.lisp
@@ -1,8 +1,8 @@
 (in-package :stash.views)
 
 (define-view posts-list-page (params)
-  (with-database (db "stash")
-    (with-collection (c "posts" db)
-      (let ((posts (mongo:find c)))
-        (loop :for p :in posts :do
-           (str (format-hash p)))))))
+  (str (header params))
+  (with-database-and-collection (c "posts" db "stash")
+    (let ((posts (mongo:find c)))
+      (loop :for p :in posts :do
+         (str (format-hash p))))))

9780c0fa41d5ca854f439874213f869556c253c4
taptap <alexey.e.egorov@gmail.com>
2016-01-22 17:45:03 +0300

    add escape-string function
    and replace cl-who:escape string with it. Because cl-who's implementation
    replaced non-latin chars with codes as well. This is very undesired, since
    UTF8 prevails now.

--- model/post.lisp
+++ model/post.lisp
@@ -7,8 +7,27 @@
    (visibility :initarg :visibility)
    (body :initarg :body)))
 
+(defun escape-string (string)
+  (with-output-to-string (s)
+    (map nil
+         (lambda (char)
+           (case char
+             ((#\<)
+              (write-sequence "&lt;" s))
+             ((#\>)
+              (write-sequence "&gt;" s))
+             ((#\&)
+              (write-sequence "&amp;" s))
+             ((#\')
+              (write-sequence "&#039;" s))
+             ((#\")
+              (write-sequence "&quot;" s))
+             (otherwise
+              (format s "~c" char))))
+         string)))
+
 (defun markdown->html (text)
   (with-output-to-string (s)
     (cl-markdown:markdown
-     (cl-who:escape-string text)
+     (escape-string text)
      :stream s)))

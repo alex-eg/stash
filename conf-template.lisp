7a35e84226eee8e91e89d5d80dd99bc3a814fedb
taptap <alexey.e.egorov@gmail.com>
2016-03-23 11:35:45 +0300

    add template config file

---
+++ conf-template.lisp
@@ -0,0 +1,12 @@
+;;; This is template. Copy to conf.lisp,
+;;; it will be read during startup.
+;;; Look at model/config.lisp for details
+
+(:domain "example.com")
+(:development nil)
+
+(:other-thing
+ (a b c d e))
+
+(:you-got-the-point?
+ (i think yes))
